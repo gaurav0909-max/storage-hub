22fd98152baa5435d370b0e44beca0fd03002a2b
Gaurav Patel <gauravp.itpath@gmail.com>
2025-01-07 15:54:30 +0530

    Searching and sorting added

--- app/(root)/[type]/page.tsx
+++ app/(root)/[type]/page.tsx
@@ -1,13 +1,19 @@
 import Card from "@/components/Card";
 import Sort from "@/components/Sort";
 import { getFiles } from "@/lib/actions/file.action";
+import { getFileTypesParams } from "@/lib/utils";
 import { Models } from "node-appwrite";
 import React from "react";
 
-const page = async ({ params }: SearchParamProps) => {
+const page = async ({ searchParams, params }: SearchParamProps) => {
   const type = ((await params)?.type as string) || "";
 
-  const files = await getFiles();
+  const searchText = ((await searchParams)?.query as string) || "";
+  const sort = ((await searchParams)?.sort as string) || "";
+
+  const types = getFileTypesParams(type) as FileType[];
+
+  const files = await getFiles({ types, searchText, sort });
   return (
     <div className="page-container">
       <section className="w-full">
@@ -19,7 +25,7 @@ const page = async ({ params }: SearchParamProps) => {
           </p>
 
           <div className="sort-container">
-            <p className="body-1 hidden sm:block text-light-200 ">Sort by:</p>
+            <p className="body-1 hidden text-light-200 sm:block">Sort by:</p>
 
             <Sort />
           </div>
